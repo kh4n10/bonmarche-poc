568b0a2dbccd407c6f21729386e76640571779f8
Tech-A55 <tech-a55@Tech-A55s-MacBook-Air.local>
2018-01-18 10:51:59 +0000

    hover works, and progressing a bit more

---
+++ features/step_definitions/my_account_page_steps.rb
@@ -0,0 +1,17 @@
+Given(/^I am on my account page$/) do
+	@bonmarche_site = BonmarcheSite.new
+	@bonmarche_site.bonmarche_my_account_page.load
+end
+
+When(/^I hover on the womens sections$/) do
+	@bonmarche_site.bonmarche_my_account_page.hover_on_women_section
+end
+
+And(/^Click on the dressing gowns option$/) do
+	sleep 2
+	@bonmarche_site.bonmarche_my_account_page.click_on_a_section
+end
+
+Then(/^I am taken to the dressing gowns page$/) do
+
+end
